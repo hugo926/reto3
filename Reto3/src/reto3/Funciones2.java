6efe135109e306096b43f20c3d68b4692b455173
danielchuperca <ciuperciucd51@gmail.com>
2024-11-14 09:39:59 +0100

    segundo commit

--- Reto3/src/reto3/Funciones2.java
+++ Reto3/src/reto3/Funciones2.java
@@ -1,5 +1,35 @@
 package reto3;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
 public class Funciones2 {
+	public static int noSonMismoAño(LocalDate fecha1, LocalDate fecha2) {
+		Scanner sc=new Scanner(System.in);
+		int cont=0;
+		int sum=0;
+		LocalDate ld1 = LocalDate.of(fecha1.getYear(),fecha1.getMonthValue() ,1);
+		LocalDate ld2 = LocalDate.of(fecha2.getYear(),fecha2.getMonthValue() ,1);
+		int meses = (int) ChronoUnit.MONTHS.between(ld1, ld2);
+
+
+
+		for (int i =0 ; i < meses ; i++) {
+			
+			sum=sum+fecha1.lengthOfMonth();
+			System.out.println(sum);
+			fecha1=fecha1.plusMonths(1);
+			cont++;
+			
+			
+			
+		}
+		int media=(sum/cont);
+		return media;
+	
+		
+	}
 
 }
--- Reto3/src/reto3/Main.java
+++ Reto3/src/reto3/Main.java
@@ -1,9 +1,30 @@
 package reto3;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Scanner sc=new Scanner(System.in);
+		LocalDate fecha1=Funciones.dimeFecha("dime primera fecha", sc);
+		LocalDate fecha2;
+		do {
+			 fecha2=Funciones.dimeFecha("dime segunda fecha ", sc);
+			
+
+			
+			
+		} while (fecha1.isAfter(fecha2));
+		 int ano1=fecha1.getYear();
+		 int ano2=fecha2.getYear();
+	
+		 if(ano1==ano2){
+			 
+		 }
+		 if(ano1!=ano2){
+			 System.out.println(Funciones2.noSonMismoAño(fecha1, fecha2));
+		 }
 
 	}
 
